235e67229f0f5d29c8e2d985079926c0f39448f9
Jiraf400 <109845546+Jiraf400@users.noreply.github.com>
2023-11-12 12:24:43 +0300

    remove app guard

--- src/expenses/expenses.module.ts
+++ src/expenses/expenses.module.ts
@@ -2,18 +2,9 @@ import { Module } from '@nestjs/common';
 import { ExpensesController } from './expenses.controller';
 import { ExpensesService } from './expenses.service';
 import { PrismaService } from '../prisma.service';
-import { APP_GUARD } from '@nestjs/core';
-import { AuthGuard } from '../auth/auth.guard';
 
 @Module({
   controllers: [ExpensesController],
-  providers: [
-    ExpensesService,
-    PrismaService,
-    {
-      provide: APP_GUARD,
-      useClass: AuthGuard,
-    },
-  ],
+  providers: [ExpensesService, PrismaService],
 })
 export class ExpensesModule {}
