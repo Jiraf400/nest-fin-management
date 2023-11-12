00778f37d5bcac881becda8093793c078f66974c
Jiraf400 <109845546+Jiraf400@users.noreply.github.com>
2023-11-12 13:21:59 +0300

    add expense category module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ExpensesModule } from './expenses/expenses.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, ExpensesModule],
+  imports: [AuthModule, UsersModule, ExpensesModule, CategoriesModule],
 })
 export class AppModule {}
