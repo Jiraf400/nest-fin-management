a4a136da9c49fe53800b873d7afa4820bec0a592
Jiraf400 <109845546+Jiraf400@users.noreply.github.com>
2023-11-15 20:15:20 +0300

    add expense mapper

--- src/expenses/expenses.module.ts
+++ src/expenses/expenses.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ExpensesController } from './expenses.controller';
 import { ExpensesService } from './expenses.service';
 import { PrismaService } from '../prisma.service';
+import { ExpenseMapper } from './mappers/expense.mapper';
 
 @Module({
   controllers: [ExpensesController],
-  providers: [ExpensesService, PrismaService],
+  providers: [ExpensesService, PrismaService, ExpenseMapper],
 })
 export class ExpensesModule {}
