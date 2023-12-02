c868770e43a644525762b4a86c90084c0ba49fa8
Jiraf400 <109845546+Jiraf400@users.noreply.github.com>
2023-12-02 14:05:34 +0300

    add limit methods create, delete, update amount

--- src/monthly-limits/monthly-limits.controller.ts
+++ src/monthly-limits/monthly-limits.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req, Res, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import { MonthlyLimitsService } from './monthly-limits.service';
+import { Request, Response } from 'express';
+import { AuthGuard } from '../auth/auth.guard';
+import { MonthlyLimitDTO } from './dto/mlimit.dto';
 
-@Controller('monthly-limits')
-export class MonthlyLimitsController {}
+@UsePipes(ValidationPipe)
+@UseGuards(AuthGuard)
+@Controller('limits')
+export class MonthlyLimitsController {
+  constructor(private mLimitsService: MonthlyLimitsService) {}
+
+  @Post()
+  async createTransaction(@Req() req: Request, @Res() res: Response, @Body() mlDto: MonthlyLimitDTO) {
+    const userFromRequest = req.body.user;
+
+    if (!userFromRequest || !mlDto) {
+      return res.status(400).json({ message: 'All fields must be filled.' });
+    }
+
+    const created = await this.mLimitsService.addNewMonthLimit(mlDto, userFromRequest);
+
+    return res.status(201).json({ status: 'OK', message: 'Successfully set limit', body: created });
+  }
+}
--- src/monthly-limits/monthly-limits.service.ts
+++ src/monthly-limits/monthly-limits.service.ts
@@ -1,9 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { MonthlyLimitDTO } from './dto/mlimit.dto';
+import { PrismaService } from '../prisma.service';
 
 @Injectable()
 export class MonthlyLimitsService {
-  //TODO method to add limit
-  //TODO method to delete limit
+  constructor(private prisma: PrismaService) {}
+
+  async addNewMonthLimit(dto: MonthlyLimitDTO, user: any) {
+    const candidate = await this.prisma.monthlyLimit.findUnique({ where: { user_id: user.sub } });
+
+    if (candidate) {
+      throw new HttpException('Monthly limit has already been set. Able only to delete or update amount', 400);
+    }
+
+    const setLimit = await this.prisma.monthlyLimit.create({
+      data: {
+        user: {
+          connect: { id: user.sub },
+        },
+        month: new Date().getMonth() + 1,
+        year: new Date().getFullYear(),
+        limit_amount: dto.limit_amount,
+        total_expenses: 0,
+      },
+    });
+
+    console.log(`set new limit ${setLimit.id}`);
+
+    return setLimit;
+  }
+
+  async addExpenseToLimit(expense_amount: number, user_id: number) {
+    const candidate = await this.prisma.monthlyLimit.findUnique({ where: { user_id: user_id } });
+
+    if (!candidate) {
+      throw new HttpException('Monthly limit object not found', 400);
+    }
+
+    const changeLimit = await this.prisma.monthlyLimit.update({
+      where: { user_id: user_id },
+      data: { total_expenses: candidate.total_expenses + expense_amount },
+    });
+
+    console.log(`change limit by adding expense amount ${expense_amount}`);
+
+    return changeLimit;
+  }
+
+  async deleteExpenseLimit(user: any) {
+    const candidate = await this.prisma.monthlyLimit.findUnique({ where: { user_id: user.sub } });
+
+    if (!candidate) {
+      throw new HttpException('No objects found', 400);
+    }
+
+    const deleteLimit = await this.prisma.monthlyLimit.delete({ where: { user_id: user.sub } });
+
+    console.log(`Delete monthly limit with id: ${deleteLimit.id}`);
+
+    return deleteLimit;
+  }
+
   //TODO method to change limit amount
   //TODO method to check if limit reached
 }
