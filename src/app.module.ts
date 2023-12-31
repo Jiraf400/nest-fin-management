b83860dc89fbe43ea12ad00127fd6dad3de0ffd8
Jiraf400 <109845546+Jiraf400@users.noreply.github.com>
2023-12-31 12:22:13 +0300

    add configService provider to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,15 @@ import { AuthModule } from './auth/auth.module';
 import { TransactionsModule } from './transactions/transactions.module';
 import { TransactionCategoriesModule } from './transaction-categories/transaction-categories.module';
 import { MonthlyLimitsModule } from './monthly-limits/monthly-limits.module';
+import { ConfigService } from './config/config.service';
 
 @Module({
   imports: [AuthModule, TransactionsModule, TransactionCategoriesModule, MonthlyLimitsModule],
+  providers: [
+    {
+      provide: ConfigService,
+      useClass: ConfigService,
+    },
+  ],
 })
 export class AppModule {}
