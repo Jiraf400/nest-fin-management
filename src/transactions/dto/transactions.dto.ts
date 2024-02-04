806985ef7eb3e56ae2f55d3bad2c2195032c6276
Bohdan Holub <sparolem234@gmail.com>
2024-02-04 10:48:41 +0300

    set minimum value to expense amount

--- src/transactions/dto/transactions.dto.ts
+++ src/transactions/dto/transactions.dto.ts
@@ -1,16 +1,17 @@
-import { IsInt, IsNotEmpty, Length } from 'class-validator';
+import { IsInt, IsNotEmpty, Length, Min } from 'class-validator';
 
 export class TransactionsDto {
-  @IsNotEmpty({ message: 'amount is required' })
-  @IsInt()
-  amount: number;
-  @IsNotEmpty()
-  @Length(3, 255)
-  description: string;
-  @IsNotEmpty()
-  @Length(3, 255)
-  type: string;
-  @IsNotEmpty()
-  @Length(3, 15)
-  category: string;
+	@IsNotEmpty({ message: 'amount is required' })
+	@IsInt()
+	@Min(1)
+	amount: number;
+	@IsNotEmpty()
+	@Length(3, 255)
+	description: string;
+	@IsNotEmpty()
+	@Length(3, 255)
+	type: string;
+	@IsNotEmpty()
+	@Length(3, 15)
+	category: string;
 }
