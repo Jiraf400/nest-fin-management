00587fca87375b9edfb682f131bcbd0c3f565c2e
Bohdan Holub <sparolem234@gmail.com>
2024-02-12 10:29:29 +0300

    fix prisma service import path

--- src/transactions/mappers/transactions.mapper.ts
+++ src/transactions/mappers/transactions.mapper.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Transaction, TransactionCategory, TransactionType, User } from '@prisma/client';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../../prisma/prisma.service';
 import { GetTransactionsDtoList } from '../dto/get-list-transactions.dto';
 import { GetTransactionDTO } from '../dto/transactions-get.dto';
 
