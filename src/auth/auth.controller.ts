5cac712adaf9c3725c93c6bf8e748c12b2bee11d
Bohdan Holub <sparolem234@gmail.com>
2024-02-13 21:44:52 +0300

    simplify error handling

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,7 +4,6 @@ import { Response } from 'express';
 import { AuthService } from './auth.service';
 import { UserLoginDto } from './dtos/user-login.dto';
 import { UserRegisterDto } from './dtos/user-register.dto';
-import { isEmailValid } from './user/isEmailValid';
 
 @Controller('auth')
 export class AuthController {
@@ -15,10 +14,6 @@ export class AuthController {
 		@Res() res: Response,
 		@Body() registerDto: UserRegisterDto,
 	): Promise<Response> {
-		if (!registerDto || !isEmailValid(registerDto.email)) {
-			return res.status(400).json({ message: 'All fields must be filled.' });
-		}
-
 		const createdUser: PrismaUser = await this.authService.register(registerDto);
 
 		return res.status(201).json({
@@ -30,10 +25,6 @@ export class AuthController {
 
 	@Post('login')
 	async loginUser(@Res() res: Response, @Body() loginDto: UserLoginDto): Promise<Response> {
-		if (!loginDto || !loginDto.password || !loginDto.email) {
-			return res.status(400).json({ message: 'All fields must be filled.' });
-		}
-
 		const access_token: string = await this.authService.login(loginDto);
 
 		return res.status(200).json({ accessToken: access_token });
