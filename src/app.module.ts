384bcef5234d174c0e201e6b993ceff71b7edf05
Bohdan Holub <sparolem234@gmail.com>
2024-02-13 21:40:15 +0300

    simplify providing of config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { TransactionsModule } from './transactions/transactions.module';
-import { TransactionCategoriesModule } from './transaction-categories/transaction-categories.module';
-import { MonthlyLimitsModule } from './monthly-limits/monthly-limits.module';
 import { ConfigService } from './config/config.service';
+import { MonthlyLimitsModule } from './monthly-limits/monthly-limits.module';
+import { TransactionCategoriesModule } from './transaction-categories/transaction-categories.module';
+import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
-  imports: [AuthModule, TransactionsModule, TransactionCategoriesModule, MonthlyLimitsModule],
-  providers: [
-    {
-      provide: ConfigService,
-      useClass: ConfigService,
-    },
-  ],
+	imports: [AuthModule, TransactionsModule, TransactionCategoriesModule, MonthlyLimitsModule],
+	providers: [ConfigService],
 })
 export class AppModule {}
